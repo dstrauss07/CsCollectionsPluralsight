6b56bfbcb1ad32289d10e075241f1c962cc216bf
dstrauss <fmdenver@gmail.com>
2019-07-07 18:43:31 -0400

    done with module 1

---
+++ src/CsharpCollections/CsharpCollections/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CsharpCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysofWeek =
+            {
+                "Monday",
+                "Tuesday",
+                "Wensday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            daysofWeek[2] = "Wednesday";
+
+            foreach (string day in daysofWeek)
+                Console.WriteLine(day);
+
+            //Console.WriteLine("Which Day do you want?");
+            //Console.Write("(Monday = 1, etc. > ");
+            //int iDay = int.Parse(Console.ReadLine());
+
+            //string chosenDay = daysofWeek[iDay - 1];
+            //Console.WriteLine($"That day is {chosenDay}");
+
+
+        }
+    }
+}
